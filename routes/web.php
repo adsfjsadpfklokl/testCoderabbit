3fac4eb4a8c4d2254cac319711005ffb9c7cde63
insaito777 <efmadecode@gmail.com>
2024-01-22 22:58:41 +0900

    routeにTestController@indexを追加

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TestController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/','TestController@index');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [TestController::class, 'index']);
