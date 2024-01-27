228fb8ef7f23563d2e633e64048ee9bd422f9288
insaito777 <efmadecode@gmail.com>
2024-01-27 18:18:43 +0900

    componentsのテスト

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,6 +10,6 @@ use Illuminate\Routing\Controller as BaseController;
 class TestController extends BaseController
 {
     public function index(){
-        return view('test');
+        return view('test',['message'=>'これはアラートメッセージです']);
     }
 }
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,1 +1,4 @@
-Hello World!
+<x-app>
+  <h1 class="text-2xl">Laravel Components</h1>
+  <x-alert :message="$message" />
+</x-app>
